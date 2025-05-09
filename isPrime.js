395e29e3351bff1c7ef01e86ee2fe72e6b3419ee
Zain Sarfraz <zainnsarfraz@gmail.com>
2025-05-09 15:00:47 +0500

    initial commit

---
+++ isPrime.js
@@ -0,0 +1,15 @@
+const isPrime = n => {
+  if (n < 2) {
+    return false;
+  }
+  for (let i = 2; i < n; i += 1) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+module.exports = {
+  isPrime,
+};
